78e6760e3ad2b3fb8c3ea508c623b12cb4b17b2b
Matti Paksula <matti.paksula@iki.fi>
2018-02-12 19:39:10 +0200

    feat (terraform) resource update support.

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "start" {
 }
 
 locals {
-  default_affinities = "${length(var.default_affinities) == 0 ?
+  default_affinities_or_none = "${length(var.default_affinities) == 0 ?
     ""
     :
     "${join(" ", formatlist("--default-affinity %s", var.default_affinities))}"
@@ -13,14 +13,14 @@ locals {
 }
 
 locals {
-  log_opts = "${length(var.log_opts) == 0 ?
+  log_opts_or_none = "${length(var.log_opts) == 0 ?
     ""
     :
     "${join(" ", formatlist("--log-opt %s", var.log_opts))}"
     }"
 }
 
-resource "null_resource" "kontena_grid_create" {
+resource "null_resource" "kontena_grid" {
   depends_on = ["null_resource.start"]
 
   provisioner "local-exec" {
@@ -30,10 +30,6 @@ kontena grid create \
   ${var.token == "" ? "" : "--token ${var.token}"} \
   ${var.subnet == "" ? "" : "--subnet ${var.subnet}"} \
   ${var.supernet == "" ? "" : "--supernet ${var.supernet}"} \
-  ${local.default_affinities} \
-  ${var.statsd_server == "" ? "" : "--statsd-server ${var.statsd_server}"} \
-  ${var.log_forwarder == "" ? "" : "--log-forwarder ${var.log_forwarder}"} \
-  ${local.log_opts} \
   ${var.name}
 EOF
   }
@@ -41,5 +37,48 @@ EOF
   provisioner "local-exec" {
     when    = "destroy"
     command = "kontena grid rm --force ${var.name}"
+  }
+}
+
+resource "null_resource" "kontena_grid_unsetter" {
+  depends_on = ["null_resource.kontena_grid"]
+
+  triggers = {
+    log_forwarder      = "${var.log_forwarder}"
+    log_opts_or_none   = "${local.log_opts_or_none}"
+    default_affinities = "${local.default_affinities_or_none}"
+    statsd_server      = "${var.statsd_server}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+kontena grid update \
+  --log-forwarder none \
+  --no-default-affinity \
+  --no-statsd-server \
+  ${var.name}
+EOF
+  }
+}
+
+resource "null_resource" "kontena_grid_update" {
+  depends_on = ["null_resource.kontena_grid_unsetter"]
+
+  triggers = {
+    log_forwarder      = "${var.log_forwarder}"
+    log_opts_or_none   = "${local.log_opts_or_none}"
+    default_affinities = "${local.default_affinities_or_none}"
+    statsd_server      = "${var.statsd_server}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+kontena grid update \
+  ${local.default_affinities_or_none} \
+  ${var.statsd_server == "" ? "" : "--statsd-server ${var.statsd_server}"} \
+  ${var.log_forwarder == "" ? "" : "--log-forwarder ${var.log_forwarder}"} \
+  ${local.log_opts_or_none} \
+  ${var.name}
+EOF
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,3 @@
-
+output "id" {
+  value = "${null_resource.kontena_grid.id}-${null_resource.kontena_grid_unsetter.id}-${null_resource.kontena_grid_update.id}"
+}
